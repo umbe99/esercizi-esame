258d9a6dddd7195d0f35a452869183249b6dd917
Gianmarco Testa <gianmarco.testa@algoritmi.eu>
2020-10-21 15:22:25 +0200

    Creazione lato BackEnd

---
+++ eserciziEsame/src/main/java/gruppoesame/eserciziEsame/EserciziEsameApplication.java
@@ -0,0 +1,13 @@
+package gruppoesame.eserciziEsame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziEsameApplication.class, args);
+	}
+
+}
---
+++ eserciziEsame/src/test/java/gruppoesame/eserciziEsame/EserciziEsameApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppoesame.eserciziEsame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EserciziEsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
