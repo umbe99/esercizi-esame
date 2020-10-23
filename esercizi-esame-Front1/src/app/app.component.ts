b5caf9bc22c4193d1f8f31fed879eee91ef2a5d3
Umberto Alivernini <umberto.alivernini@algoritmi.eu>
2020-10-23 17:37:52 +0200

    aggiornamento

--- esercizi-esame-Front1/src/app/app.component.ts
+++ esercizi-esame-Front1/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contatto } from './contatto';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'esercizi-esame-Front1';
+  //readonly urlHost="http://localost:8080";
+
+
+  aggiungiNome:string="";
+  aggiungiCognome:string="";
+  aggiungiTelefono:string="";
+  constructor(private http:HttpClient){
+
+  }
+
+
+
+
+
+
+
+  ngOnInit(): void{}
+
+
+
+
+  //aggiungi(nome:string,cognome:string,telefono:string){
+
+ // }
+  //modifica(){}
+  //elimina(){}
+  //lista(){}
+
 }
---
+++ esercizi-esame-Front1/src/app/contatto.ts
@@ -0,0 +1,14 @@
+export class Contatto{
+    id:number;
+    nome:string;
+    cognome:string;
+    telefono:string;
+
+    constructor(_id:number,_nome:string,_cognome:string,_telefono:string){
+        this.id=_id;
+        this.nome=_nome;
+        this.cognome=_cognome;
+        this.telefono=_telefono;
+    }
+    
+}
